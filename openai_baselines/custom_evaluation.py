820710d111472509a5d741013257b853762d3db4
robinhenry <robin.henry012@gmail.com>
2020-09-26 18:07:03 +0200

    td3 script ready to run

---
+++ openai_baselines/custom_evaluation.py
@@ -0,0 +1,221 @@
+"""
+This function `evaluate_policy` was copied from https://github.com/DLR-RM/stable-baselines3/blob/8b16324ba76e4264d071959692ec93a3f334b477/stable_baselines3/common/evaluation.py#L13
+and the class `EvalCallback` was copied from https://github.com/DLR-RM/stable-baselines3/blob/8b16324ba76e4264d071959692ec93a3f334b477/stable_baselines3/common/callbacks.py.
+They were both modified so as to introduce an additional parameter to specify
+the maximum length of each episode and to compute discounted returns instead
+of sums of rewards.
+"""
+
+import os
+import typing
+from typing import Callable, List, Optional, Tuple, Union, Dict, Any
+import gym
+import numpy as np
+import warnings
+
+from stable_baselines3.common.callbacks import EventCallback, BaseCallback
+from stable_baselines3.common.vec_env import DummyVecEnv, VecEnv, \
+    sync_envs_normalization
+
+if typing.TYPE_CHECKING:
+    from stable_baselines3.common.base_class import BaseAlgorithm
+
+
+def evaluate_policy(
+    model: "BaseAlgorithm",
+    env: Union[gym.Env, VecEnv],
+    n_eval_episodes: int = 10,
+    deterministic: bool = True,
+    render: bool = False,
+    callback: Optional[Callable] = None,
+    reward_threshold: Optional[float] = None,
+    return_episode_rewards: bool = False,
+    max_steps_per_episode: int = None,
+    gamma: float = 0.99
+) -> Union[Tuple[float, float], Tuple[List[float], List[int]]]:
+    """
+    Runs policy for ``n_eval_episodes`` episodes and returns average reward.
+    This is made to work only with one env.
+    :param model: (BaseAlgorithm) The RL agent you want to evaluate.
+    :param env: (gym.Env or VecEnv) The gym environment. In the case of a ``VecEnv``
+        this must contain only one environment.
+    :param n_eval_episodes: (int) Number of episode to evaluate the agent
+    :param deterministic: (bool) Whether to use deterministic or stochastic actions
+    :param render: (bool) Whether to render the environment or not
+    :param callback: (callable) callback function to do additional checks,
+        called after each step.
+    :param reward_threshold: (float) Minimum expected reward per episode,
+        this will raise an error if the performance is not met
+    :param return_episode_rewards: (Optional[float]) If True, a list of reward per episode
+        will be returned instead of the mean.
+    :param max_steps_per_episode: (int) The maximum number of steps per episode.
+    :param gamma: (float) The discount factor.
+    :return: (float, float) Mean reward per episode, std of reward per episode
+        returns ([float], [int]) when ``return_episode_rewards`` is True
+    """
+    if isinstance(env, VecEnv):
+        assert env.num_envs == 1, "You must pass only one environment when using this function"
+
+    if max_steps_per_episode is None:
+        max_steps = np.inf
+
+    episode_rewards, episode_lengths = [], []
+    for i in range(n_eval_episodes):
+        # Avoid double reset, as VecEnv are reset automatically
+        if not isinstance(env, VecEnv) or i == 0:
+            obs = env.reset()
+        done, state = False, None
+        episode_reward = 0.0
+        episode_length = 0
+        while not done and episode_length < max_steps_per_episode:
+            action, state = model.predict(obs, state=state, deterministic=deterministic)
+            obs, reward, done, _info = env.step(action)
+            episode_reward += reward * (gamma ** episode_length)
+            if callback is not None:
+                callback(locals(), globals())
+            episode_length += 1
+            if render:
+                env.render()
+        episode_rewards.append(episode_reward)
+        episode_lengths.append(episode_length)
+    mean_reward = np.mean(episode_rewards)
+    std_reward = np.std(episode_rewards)
+    if reward_threshold is not None:
+        assert mean_reward > reward_threshold, "Mean reward below threshold: " f"{mean_reward:.2f} < {reward_threshold:.2f}"
+    if return_episode_rewards:
+        return episode_rewards, episode_lengths
+    return mean_reward, std_reward
+
+
+
+class EvalCallback(EventCallback):
+    """
+    Callback for evaluating an agent.
+    :param eval_env: (Union[gym.Env, VecEnv]) The environment used for initialization
+    :param callback_on_new_best: (Optional[BaseCallback]) Callback to trigger
+        when there is a new best model according to the ``mean_reward``
+    :param n_eval_episodes: (int) The number of episodes to test the agent
+    :param eval_freq: (int) Evaluate the agent every eval_freq call of the callback.
+    :param log_path: (str) Path to a folder where the evaluations (``evaluations.npz``)
+        will be saved. It will be updated at each evaluation.
+    :param best_model_save_path: (str) Path to a folder where the best model
+        according to performance on the eval env will be saved.
+    :param deterministic: (bool) Whether the evaluation should
+        use a stochastic or deterministic actions.
+    :param deterministic: (bool) Whether to render or not the environment during evaluation
+    :param render: (bool) Whether to render or not the environment during evaluation
+    :param verbose: (int)
+    :param max_steps_per_episode: (int) The maximum number of steps per episode.
+    :param gamma: (float) The discount factor.
+    """
+
+    def __init__(
+        self,
+        eval_env: Union[gym.Env, VecEnv],
+        callback_on_new_best: Optional[BaseCallback] = None,
+        n_eval_episodes: int = 5,
+        eval_freq: int = 10000,
+        log_path: str = None,
+        best_model_save_path: str = None,
+        deterministic: bool = True,
+        render: bool = False,
+        verbose: int = 1,
+        max_steps_per_episode: int = None,
+        gamma: float = 0.99
+    ):
+        super(EvalCallback, self).__init__(callback_on_new_best, verbose=verbose)
+        self.n_eval_episodes = n_eval_episodes
+        self.eval_freq = eval_freq
+        self.best_mean_reward = -np.inf
+        self.last_mean_reward = -np.inf
+        self.deterministic = deterministic
+        self.render = render
+        self.max_steps_per_episode = max_steps_per_episode
+        self.gamma = gamma
+
+        # Convert to VecEnv for consistency
+        if not isinstance(eval_env, VecEnv):
+            eval_env = DummyVecEnv([lambda: eval_env])
+
+        if isinstance(eval_env, VecEnv):
+            assert eval_env.num_envs == 1, "You must pass only one environment for evaluation"
+
+        self.eval_env = eval_env
+        self.best_model_save_path = best_model_save_path
+        # Logs will be written in ``evaluations.npz``
+        if log_path is not None:
+            log_path = os.path.join(log_path, "evaluations")
+        self.log_path = log_path
+        self.evaluations_results = []
+        self.evaluations_timesteps = []
+        self.evaluations_length = []
+
+    def _init_callback(self):
+        # Does not work in some corner cases, where the wrapper is not the same
+        if not isinstance(self.training_env, type(self.eval_env)):
+            warnings.warn("Training and eval env are not of the same type" f"{self.training_env} != {self.eval_env}")
+
+        # Create folders if needed
+        if self.best_model_save_path is not None:
+            os.makedirs(self.best_model_save_path, exist_ok=True)
+        if self.log_path is not None:
+            os.makedirs(os.path.dirname(self.log_path), exist_ok=True)
+
+    def _on_step(self) -> bool:
+
+        if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
+            # Sync training and eval env if there is VecNormalize
+            sync_envs_normalization(self.training_env, self.eval_env)
+
+            episode_rewards, episode_lengths = evaluate_policy(
+                self.model,
+                self.eval_env,
+                n_eval_episodes=self.n_eval_episodes,
+                render=self.render,
+                deterministic=self.deterministic,
+                return_episode_rewards=True,
+                max_steps_per_episode=self.max_steps_per_episode,
+                gamma=self.gamma
+            )
+
+            if self.log_path is not None:
+                self.evaluations_timesteps.append(self.num_timesteps)
+                self.evaluations_results.append(episode_rewards)
+                self.evaluations_length.append(episode_lengths)
+                np.savez(
+                    self.log_path,
+                    timesteps=self.evaluations_timesteps,
+                    results=self.evaluations_results,
+                    ep_lengths=self.evaluations_length,
+                )
+
+            mean_reward, std_reward = np.mean(episode_rewards), np.std(episode_rewards)
+            mean_ep_length, std_ep_length = np.mean(episode_lengths), np.std(episode_lengths)
+            self.last_mean_reward = mean_reward
+
+            if self.verbose > 0:
+                print(f"Eval num_timesteps={self.num_timesteps}, " f"episode_reward={mean_reward:.2f} +/- {std_reward:.2f}")
+                print(f"Episode length: {mean_ep_length:.2f} +/- {std_ep_length:.2f}")
+            # Add to current Logger
+            self.logger.record("eval/mean_reward", float(mean_reward))
+            self.logger.record("eval/mean_ep_length", mean_ep_length)
+
+            if mean_reward > self.best_mean_reward:
+                if self.verbose > 0:
+                    print("New best mean reward!")
+                if self.best_model_save_path is not None:
+                    self.model.save(os.path.join(self.best_model_save_path, "best_model"))
+                self.best_mean_reward = mean_reward
+                # Trigger callback if needed
+                if self.callback is not None:
+                    return self._on_event()
+
+        return True
+
+    def update_child_locals(self, locals_: Dict[str, Any]) -> None:
+        """
+        Update the references to the local variables.
+        :param locals_: (Dict[str, Any]) the local variables during rollout collection
+        """
+        if self.callback:
+            self.callback.update_locals(locals_)
