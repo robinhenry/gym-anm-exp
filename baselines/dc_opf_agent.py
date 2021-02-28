8ad1beacd4d05be515d16f0af2d65ae4f970d9d5
Robin <robin.henry012@gmail.com>
2021-02-28 14:57:53 +0100

    changed file name in baseline scripts

--- baselines/dc_opf_agent.py
+++ baselines/dc_opf_agent.py
@@ -5,10 +5,10 @@ from baselines.run_baseline import run_baseline
 def dcopf_grid_search():
     T = 3000
     seed = 1000
-    savefile = f'./DCOPF_returns_T{T}.txt'
+    savefile = f'./DCOPF_returns_T{T}_new_obj.txt'
 
-    for planning_steps in [8, 16, 32, 64, 128]:
-        for safety_margin in [0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99, 1.]:
+    for planning_steps in [8, 16, 32, 64]:
+        for safety_margin in [0.92, 0.94, 0.96, 0.98, 1.0]:
 
             run_baseline(DCOPFAgent, safety_margin, planning_steps, T,
                          seed, savefile)
--- baselines/mpc_agent.py
+++ baselines/mpc_agent.py
@@ -5,10 +5,10 @@ from baselines.run_baseline import run_baseline
 def mpc_grid_search():
     T = 3000
     seed = 1000
-    savefile = f'./MPC_returns_T{T}.txt'
+    savefile = f'./MPC_returns_T{T}_new_obj.txt'
 
-    for planning_steps in [8, 16, 32, 64, 128, 256]:
-        for safety_margin in [0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99, 1.]:
+    for planning_steps in [8, 16, 32, 64, 128]:
+        for safety_margin in [0.92, 0.94, 0.96, 0.98, 1.]:
 
             run_baseline(MPCAgentANM6Easy, safety_margin, planning_steps, T,
                          seed, savefile)
